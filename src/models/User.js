94ba4e907d6d392519256d1f20b0224068ec716d
Your Name <you@example.com>
2018-06-18 19:37:28 +0400

    sign up server-side record

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,31 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import uniqueValidator from "mongoose-unique-validator";
+
 // This is a database schema of user
 // TODO: add uniqueness of email
 const schema = new Schema(
   {
-    email: { type: String, required: true, lowercase: true, index: true },
-    passwordHash: { type: String, required: true }
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      index: true,
+      unique: true
+    },
+    passwordHash: { type: String, required: true },
+    confirmed: { type: Boolean, default: false }
   },
   { timestamps: true }
 );
 
 schema.methods.isValidPasswords = function isValidPasswords(password) {
   return bcrypt.compareSync(password, this.passwordHash);
+};
+
+schema.methods.setPassword = function setPassword(password) {
+  this.passwordHash = bcrypt.hashSync(password, 10);
 };
 
 schema.methods.generateJWT = function generateJWT() {
@@ -27,8 +40,10 @@ schema.methods.generateJWT = function generateJWT() {
 schema.methods.toAuthJSON = function toAuthJSON() {
   return {
     email: this.email,
-    token: this.generateJWT
+    token: this.generateJWT,
+    confirmed: this.confirmed
   };
 };
 
+schema.plugin(uniqueValidator, { message: "This email is alread taken" });
 export default mongoose.model("User", schema);
