150ed89acd307684b1e694fa2fd5d4cd4009b5db
Your Name <you@example.com>
2018-06-21 13:45:57 +0400

    Implemented Confirmation mailer

--- src/models/User.js
+++ src/models/User.js
@@ -15,7 +15,8 @@ const schema = new Schema(
       unique: true
     },
     passwordHash: { type: String, required: true },
-    confirmed: { type: Boolean, default: false }
+    confirmed: { type: Boolean, default: false },
+    confirmationToken: { type: String, default: "" }
   },
   { timestamps: true }
 );
@@ -28,10 +29,18 @@ schema.methods.setPassword = function setPassword(password) {
   this.passwordHash = bcrypt.hashSync(password, 10);
 };
 
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+  this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+  return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
+};
 schema.methods.generateJWT = function generateJWT() {
   return jwt.sign(
     {
-      email: this.email
+      email: this.email,
+      confirmed: this.confirmed
     },
     process.env.JWT_SECRET
   );
@@ -40,7 +49,7 @@ schema.methods.generateJWT = function generateJWT() {
 schema.methods.toAuthJSON = function toAuthJSON() {
   return {
     email: this.email,
-    token: this.generateJWT,
+    token: this.generateJWT(),
     confirmed: this.confirmed
   };
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,12 +6,32 @@ const router = express.Router();
 router.post("/", (req, res) => {
   const { credentials } = req.body;
   User.findOne({ email: credentials.email }).then(user => {
-    if (user && user.isValidPasswords(credentials.passwords)) {
-      req.json({ user: user.toAuthJSON() });
-    } else {
-      res.status(400).json({ errors: { global: "Invalid Credential" } });
+    if (user && user.isValidPasswords(credentials.password)) {
+      // console.log({ user: user.toAuthJSON() });
+      return res.json({ user: user.toAuthJSON() });
     }
+    return res.status(400).json({ errors: { global: "Invalid Credential" } });
   });
+});
+
+router.post("/confirmation", (req, res) => {
+  const token = req.body.token;
+  console.log("ager a", token);
+  User.findOneAndUpdate(
+    {
+      confirmationToken: token
+    },
+    {
+      confirmationToken: "",
+      confirmed: true
+    },
+    {
+      new: true
+    }
+  ).then(
+    user =>
+      user ? res.json({ user: user.toAuthJSON() }) : res.status(400).json({})
+  );
 });
 
 export default router;
