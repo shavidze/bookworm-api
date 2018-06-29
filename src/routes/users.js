46ad2b4a7ccf3bff059d9fe8af7c1069bc64c6a3
Your Name <you@example.com>
2018-06-29 13:44:28 +0400

    currentUser api end point

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User";
 import parseErrors from "../utils/parseErrors";
+import authenticate from "../middlewares/authenticate";
 import { sendConfirmationEmail } from "../mailer";
 
 const router = express.Router();
@@ -17,6 +18,15 @@ router.post("/", (req, res) => {
       res.json({ user: userRecord.toAuthJSON() });
     })
     .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
+});
+
+router.get("/current_user", authenticate, (req, res) => {
+  res.json({
+    user: {
+      email: req.currentUser.email,
+      confirmed: req.currentUser.confirmed
+    }
+  });
 });
 
 export default router;
